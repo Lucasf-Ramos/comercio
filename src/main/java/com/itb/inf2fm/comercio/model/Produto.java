70c1263cb7af8cd187a5e720b74a858c1ee52ffb
Lucas-Ramos <drawingworldab@gmail.com>
2023-06-06 12:29:13 -0300

    inicio controle de versão

---
+++ src/main/java/com/itb/inf2fm/comercio/model/Produto.java
@@ -0,0 +1,16 @@
+package com.itb.inf2fm.comercio.model;
+
+public class Produto {
+	
+	private Long id;
+	private String nome;
+	private String descricao;
+	private String codigoBarras;
+	private double proco;
+	
+	// public : acesso liberado para todas as classes.
+	// private: acesso permite apenas os membros
+	//			da propria classe
+	//			esntende-se por membros(atributos e ou metodos)
+	
+}
